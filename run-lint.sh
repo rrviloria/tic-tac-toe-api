ed09ba567ed6be730a2b3c99ac1969f3e44646e4
Raymund Viloria <raymund@epicode.nl>
2019-09-07 19:13:07 +0800

    Fix linting and test

--- run-lint.sh
+++ run-lint.sh
@@ -4,6 +4,7 @@
 # Shell script for running tests inside a container
 
 # This is it make sure that the code follows PEP8 standard
+# PEP8 reference https://realpython.com/python-pep8/
 
 >&2 echo "\n\n\n*** Running lint ***\n\n\n"
-docker exec -it raymundtictactoe_web_1 pipenv run flake8 --statistic --exclude='migrations/' --extend-ignore=F401 --ignore=E501,E402
+docker exec -it tictactoeapi_web_1 pipenv run flake8 --statistic --exclude='*/migrations/*' --extend-ignore=F401
